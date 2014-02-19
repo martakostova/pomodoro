3b3bc833fa9d68d7b0800ea3465063169eaaa0f9
Yitz Jacob <me@yitz.com>
2014-02-20 01:25:41 +0200

    find and replace of $pomodoroName to $timerName
    I don’t have the right signing credentials to test the changes, so I’m
    offering it up as is. Of course this would’ve been trivial for the
    original dev to do himself.

--- Pomodoro/src/CommonController.m
+++ Pomodoro/src/CommonController.m
@@ -45,7 +45,7 @@
 }
 
 - (NSString*) bindCommonVariables:(NSString*)name {
-	NSArray* variables = [NSArray arrayWithObjects:@"$pomodoroName", @"$duration", @"$dailyPomodoroDone", @"$globalPomodoroDone",nil];
+	NSArray* variables = [NSArray arrayWithObjects:@"$timerName", @"$duration", @"$dailyPomodoroDone", @"$globalPomodoroDone",nil];
 	NSString* durationString = [[[NSUserDefaults standardUserDefaults] objectForKey:@"initialTime"] stringValue];
 	NSString* dailyPomodoroDone = [[[NSUserDefaults standardUserDefaults] objectForKey:@"dailyPomodoroDone"] stringValue];
 	NSString* globalPomodoroDone = [[[NSUserDefaults standardUserDefaults] objectForKey:@"globalPomodoroDone"] stringValue];
